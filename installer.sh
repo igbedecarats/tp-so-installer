3d54395170b6403ef3ff468c9f7e564ea22aecfa
Ivan Bedecarats <igbedecarats@gmail.com>
2014-04-27 14:48:01 -0300

    * Added logging in installer.sh * Added logging.sh to be used as an example

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BASEDIR='puto'
+BASEDIR_GRUPO=''
 CONFDIR=''
 BINDIR=''
 MAEDIR=''
@@ -14,14 +14,35 @@ ESTADO_INSTALACION=''
 ACEPTO_TERMINOS_Y_CONDICIONES='no'
 
 get_base_dir() {
-	BASEDIR=$PWD
-	CONFDIR='$BASEDIR/CONFDIR'
+	BASEDIR_GRUPO=$PWD
+	CONFDIR=$BASEDIR_GRUPO/conf
 }
 
-read -p "Ingrese un numero: "
-echo "El numero ingresado es:" $REPLY
+make_confdir_folder() {
+	if [ -d "$CONFDIR" ]
+	then
+		echo "$CONFDIR found."
+	else
+		echo "$CONFDIR not found. Creating it!"
+		mkdir $CONFDIR
+	fi
+}
+
+init_installer_log_file() {
+	if [ -d "$CONFDIR" ]
+	then
+		touch $CONFDIR/Installer.log
+		#echo 'date "+%m-%d-%y %T" - Archivo $CONFDIR/Installer.log creado.' >> $CONFDIR/Installer.log
+		echo "date '+%m-%d-%y %T' - Archivo $CONFDIR/Installer.log creado." >> $CONFDIR/Installer.log
+	fi
+}
+
+#read -p "Ingrese un numero: "
+#echo "El numero ingresado es:" $REPLY
 get_base_dir
-echo $BASEDIR
+echo $BASEDIR_GRUPO
 echo $CONFDIR
+make_confdir_folder
+init_installer_log_file
 
 

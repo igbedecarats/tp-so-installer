c091286077f74ef9fa21d66a642915fa71fa6086
Ivan Bedecarats <igbedecarats@gmail.com>
2014-04-27 02:14:02 -0300

    * Added first attempt of the script * Added enunciados

---
+++ installer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+BASEDIR='puto'
+CONFDIR=''
+BINDIR=''
+MAEDIR=''
+NOVEDIR=''
+ACEPDIR=''
+INFODIR=''
+RECHDIR=''
+LOGDIR=''
+CANT_COMP_FALTANTES=''
+ESTADO_INSTALACION=''
+ACEPTO_TERMINOS_Y_CONDICIONES='no'
+
+get_base_dir() {
+	BASEDIR=$PWD
+	CONFDIR='$BASEDIR/CONFDIR'
+}
+
+get_base_dir
+echo $BASEDIR
+echo $CONFDIR
+
+
